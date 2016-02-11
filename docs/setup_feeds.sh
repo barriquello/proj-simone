9fee5f1122f38265a978e81501cd0c2c49e72e8a
barriquello <barriquello@gmail.com>
2016-02-11 17:27:19 -0200

    adicionado projeto shield

---
+++ docs/setup_feeds.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## create feeds
+
+#SERVER=http://emon-gpsnetcms.rhcloud.com
+SERVER=http://simon-gpsnetcms.rhcloud.com
+APIKEY=a4b245ca16c183c2ad2286efb342d09a
+REQUEST=$SERVER/feed/list.json\&apikey=$APIKEY
+echo $REQUEST
+curl $REQUEST
+
+declare -a feednames=("fTE" "fTO" "fVcn" "fVbn" "fVan" "fVca" "fVbc" "fVab" "fIPc" "fIPb" "fIPa" "fIMc" "fIMb" "fIMa" \
+"fIc" "fIa" "fIb" "fPQ" "fPS" "fPP" "fQM" "fSM" "fPM" "fFreq" "fPF" "fQ" "fS" "fP" "fEQ" "fES" "fEP" "fVL" "fNO") 
+
+for f in "${feednames[@]}"
+do
+   echo "$f"
+   REQUEST=$SERVER/feed/create.json?name=$f\&datatype=1\&engine=2\&options={"interval":30}\&apikey=$APIKEY
+   echo $REQUEST
+   curl $REQUEST
+done
+
+REQUEST=$SERVER/feed/list.json\&apikey=$APIKEY
+echo $REQUEST
+curl $REQUEST
